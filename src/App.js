b62b8b7793a688c771ae7c0ad4dc3b1aad08247c
onkarlagad0602 <onkarlagad0602@gmail.com>
2024-04-11 21:21:44 +0530

    added checkout page with quantity buttons and dynamic total

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Routes, Route, Outlet} from 'react-router-dom'
 import Authentication from './routes/authentication/authentication.component'
 import Navigation from './routes/navigation/navigation.component'
 import Shop from './routes/shop/shop.component'
+import Checkout from './routes/checkout/checkout.component'
 const App = () => {
 
   return (
@@ -11,6 +12,8 @@ const App = () => {
      <Route index element={<Home/>}/>
      <Route path='shop' element={<Shop/>}/>
      <Route path='auth' element={<Authentication/>}/>
+     <Route path='checkout' element={<Checkout/>}/>
+
 
 
      </Route>
